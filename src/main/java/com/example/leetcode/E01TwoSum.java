e99f4885550cde6ba223b630cc91f72444e20f46
wuyingbin <yb.ng@foxmail.com>
2022-04-02 17:25:43 +0800

    feat: 题目
    1. 两数之和
    20. 有效的括号

---
+++ src/main/java/com/example/leetcode/E01TwoSum.java
@@ -0,0 +1,72 @@
+package com.example.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <br/>
+ * 示例:
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * <br/>
+ * @author WuYingBin
+ * date: 2022/4/2
+ */
+public class E01TwoSum {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum1(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSum2(new int[]{2, 7, 11, 15}, 9)));
+    }
+
+    /**
+     * 方法一：
+     * 暴力法
+     * 通过两次循环来找出那两个索引
+     * <br/>
+     * 时间：69ms，内存：36.5M
+     *
+     * @param nums   数组
+     * @param target 目标值
+     * @return 两个索引
+     */
+    public static int[] twoSum1(int[] nums, int target) {
+        for (int i = 0, length = nums.length; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    /**
+     * 方法二：
+     * 利用HashMap查询的高效率特点，把查询工作从数组转到HashMap上
+     * 1.HashMap存储结构<值, 索引>
+     * 2.执行一次循环
+     * 3.边循环边查找map中有没有一个键加上当前数等于target的，
+     * 4.如果有，返回对应键的值（也就是这个数在原数组中的索引）和当前循环循环到的索引。
+     * 5.如果没有，把当前数按<值, 索引>的规则插入map中
+     * <br>
+     * 时间：3ms，内存：38M
+     *
+     * @param nums   数组
+     * @param target 目标值
+     * @return 两个索引
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(16);
+        for (int i = 0, length = nums.length; i < length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
---
+++ src/main/java/com/example/leetcode/E20ValidParentheses.java
@@ -0,0 +1,123 @@
+package com.example.leetcode;
+
+import java.util.*;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * <br/>
+ * 有效字符串需满足：
+ * <br/>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * <br/>
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * <br/>
+ * 输入：s = "([)]"
+ * 输出：false
+ * <br/>
+ * @author WuYingBin
+ * date: 2022/4/2
+ */
+public class E20ValidParentheses {
+    public static void main(String[] args) {
+        System.out.println(isValid1("{[]}"));
+        System.out.println(isValid2("{[]}"));
+        System.out.println(isValid3("{[]}"));
+    }
+
+    /**
+     * 方法一：
+     * 暴力法，不断去除成对的括号
+     * <br/>
+     * 时间：114ms 6.07%
+     * 内存：37M   5.01%
+     * <br/>
+     *
+     * @param s 输入的符号
+     * @return 符号是否匹配
+     */
+    public static boolean isValid1(String s) {
+        while (s.contains("()") || s.contains("[]") || s.contains("{}")) {
+            s = s.replaceAll("\\(\\)", "");
+            s = s.replaceAll("\\[]", "");
+            s = s.replaceAll("\\{}", "");
+        }
+        return Objects.equals(s, "");
+    }
+
+    /**
+     * 方法二：
+     * 核心判断方法：isSym，看两个字符是否匹配
+     * 1.准备一个字符类型的栈
+     * 2.按字符遍历
+     * 3.如果栈是空的，直接入栈
+     * 4.如果栈不是空的，匹配栈中的字符和当前遍历的字符，匹配成功则出栈；否则入栈
+     * 5.遍历结束后，判断栈是否为空，空的话说明所有符号都能正常匹配、出栈；否则有符号不能正常匹配
+     * <br/>
+     * 时间：2ms   54.06%
+     * 内存：39.5M 25.80%
+     * <br/>
+     *
+     * @param s 输入的符号
+     * @return 符号是否正确闭合
+     */
+    public static boolean isValid2(String s) {
+        Deque<Character> deque = new ArrayDeque<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            Character first = deque.peekFirst();
+            if (deque.isEmpty()) {
+                deque.offerFirst(c);
+            } else if ((first == '(' && c == ')')
+                    || (first == '[' && c == ']')
+                    || (first == '{' && c == '}')) {
+                deque.pollFirst();
+            } else {
+                deque.offerFirst(c);
+            }
+        }
+        return deque.isEmpty();
+    }
+
+
+    /**
+     * 方法三：
+     * 缓存右括号与左括号的映射关系
+     * 迭代字符串
+     * 如果当前值是左括号，则添加到栈
+     * 如果当前值是右括号，则检查栈顶元素是否是与之对应的左括号
+     *  如果是，则移除栈顶左括号
+     *  否则直接返回false
+     * 迭代到最后检查栈中是否还有没完成匹配的元素，如果有代表匹配失败
+     * <br/>
+     * 时间：2ms   54.06%
+     * 内存：39.6M 19.78%
+     * <br/>
+     *
+     * @param s 输入的符号
+     * @return 符号是否匹配
+     */
+    public static boolean isValid3(String s) {
+        Map<Character, Character> map = new HashMap<>(16);
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        Deque<Character> deque = new ArrayDeque<>();
+        char[] charArray = s.toCharArray();
+        for (char item : charArray) {
+            if (map.containsKey(item)) {
+                //是右符号
+                if (map.get(item).equals(deque.peekFirst())) {
+                    deque.pollFirst();
+                } else {
+                    return false;
+                }
+            } else {
+                //是左符号
+                deque.offerFirst(item);
+            }
+        }
+        return deque.isEmpty();
+    }
+}

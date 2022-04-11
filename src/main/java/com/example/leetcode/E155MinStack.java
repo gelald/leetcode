d2e831f1cd77e97afc0c58bd542be92a115aeeca
wuyingbin <yb.ng@foxmail.com>
2022-04-11 09:50:13 +0800

    feat: 155.最小栈
    2.辅助栈
    3.存储当前元素与插入后最小值的数组结构

--- src/main/java/com/example/leetcode/E155MinStack.java
+++ src/main/java/com/example/leetcode/E155MinStack.java
@@ -2,15 +2,43 @@ package com.example.leetcode;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * 设计一个支持 push pop top 等操作,并在在常数时间内检索到最小元素的栈
+ *
  * @author WuYingBin
  * date: 2022/4/8
  */
 public class E155MinStack {
     public static void main(String[] args) {
-        MinStack minStack = new MinStack();
+        /*MinStack1 minStack = new MinStack1();
+        minStack.push(-2);
+        System.out.println("minStack.push(-2) ==> " + minStack.deque);
+        minStack.push(0);
+        System.out.println("minStack.push(0) ==> " + minStack.deque);
+        minStack.push(-3);
+        System.out.println("minStack.push(-3) ==> " + minStack.deque);
+        System.out.println("minStack.getMin() ==> " + minStack.getMin());
+        minStack.pop();
+        System.out.println("minStack.pop() ==> " + minStack.deque);
+        System.out.println("minStack.top() ==> " + minStack.top());
+        System.out.println("minStack.getMin() ==> " + minStack.getMin());*/
+
+        /*MinStack2 minStack = new MinStack2();
+        minStack.push(-2);
+        System.out.println("minStack.push(-2) ==> " + minStack.deque);
+        minStack.push(0);
+        System.out.println("minStack.push(0) ==> " + minStack.deque);
+        minStack.push(-3);
+        System.out.println("minStack.push(-3) ==> " + minStack.deque);
+        System.out.println("minStack.getMin() ==> " + minStack.getMin());
+        minStack.pop();
+        System.out.println("minStack.pop() ==> " + minStack.deque);
+        System.out.println("minStack.top() ==> " + minStack.top());
+        System.out.println("minStack.getMin() ==> " + minStack.getMin());*/
+
+        MinStack3 minStack = new MinStack3();
         minStack.push(-2);
         System.out.println("minStack.push(-2) ==> " + minStack.deque);
         minStack.push(0);
@@ -24,18 +52,28 @@ public class E155MinStack {
         System.out.println("minStack.getMin() ==> " + minStack.getMin());
     }
 
-    static class MinStack {
+    /**
+     * 每次push、pop操作都手动实现最小值的查询
+     * <br/>
+     * 时间: 4ms   94.39%
+     * 内存: 43MB  57.70%
+     * <br/>
+     */
+    static class MinStack1 {
         public Deque<Integer> deque;
         public int min;
+
         /**
          * 初始化堆栈对象
          */
-        public MinStack() {
+        public MinStack1() {
             this.deque = new ArrayDeque<>();
         }
 
         /**
          * 将元素x推入栈
+         * 每次插入后,都需要确定最小元素
+         *
          * @param x 元素
          */
         public void push(int x) {
@@ -51,6 +89,9 @@ public class E155MinStack {
 
         /**
          * 删除堆栈顶部元素
+         * 如果删除的不是最小元素,直接删除
+         * 如果删除的恰好是最小元素,那么在删除后要重新寻找最小元素
+         * 如果删除后栈空了,那么最小元素就是默认值
          */
         public void pop() {
             Integer peek = this.deque.peek();
@@ -61,6 +102,10 @@ public class E155MinStack {
                 this.deque.pollFirst();
             } else {
                 this.deque.pollFirst();
+                if (this.deque.isEmpty()) {
+                    this.min = 0;
+                    return;
+                }
                 peek = this.deque.peek();
                 if (peek == null) {
                     throw new RuntimeException();
@@ -77,6 +122,7 @@ public class E155MinStack {
 
         /**
          * 获取堆栈顶部元素
+         *
          * @return 堆栈顶部的元素
          */
         public int top() {
@@ -89,10 +135,87 @@ public class E155MinStack {
 
         /**
          * 获取堆栈中最小元素
+         *
          * @return 最小元素
          */
         public int getMin() {
             return this.min;
+        }
+    }
+
+    /**
+     * 辅助栈，每次插入元素时都同时插入当前最小值到辅助栈内
+     * <br/>
+     * 时间: O(1)  4ms  94.39%
+     * 内存: O(n)  43.2MB  41.02%
+     * <br/>
+     */
+    static class MinStack2 {
+        public Deque<Integer> deque;
+        public Deque<Integer> minDeque;
+
+        public MinStack2() {
+            this.deque = new LinkedList<>();
+            this.minDeque = new LinkedList<>();
+            this.minDeque.push(Integer.MAX_VALUE);
+        }
+
+        public void push(int x) {
+            if (this.deque.isEmpty()) {
+                this.deque.push(x);
+                this.minDeque.push(x);
+            } else {
+                this.deque.push(x);
+                this.minDeque.push(Math.min(x, this.minDeque.getFirst()));
+            }
+        }
+
+        public void pop() {
+            this.deque.poll();
+            this.minDeque.poll();
+        }
+
+        public int top() {
+            return this.deque.getFirst();
+        }
+
+        public int getMin() {
+            return this.minDeque.getFirst();
+        }
+    }
+
+    /**
+     * 栈中存储的结构为[当前元素，插入元素后的最小值]
+     * <br/>
+     * 时间: O(1)  4ms  94.39%
+     * 内存: O(n)  42.9MB  70.94%
+     * <br/>
+     */
+    static class MinStack3 {
+        public Deque<Integer[]> deque;
+
+        public MinStack3() {
+            this.deque = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            if (this.deque.isEmpty()) {
+                this.deque.push(new Integer[]{x, x});
+            } else {
+                this.deque.push(new Integer[]{x, Math.min(x, this.deque.getFirst()[1])});
+            }
+        }
+
+        public void pop() {
+            this.deque.poll();
+        }
+
+        public int top() {
+            return this.deque.getFirst()[0];
+        }
+
+        public int getMin() {
+            return this.deque.getFirst()[1];
         }
     }
 }
